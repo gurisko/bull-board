699173dd956d76a1522f7084d53a545a51d835c2
gurisko <contact@matusgura.com>
2025-02-05 14:55:11 +0100

    chore: format @bull-board/nestjs

--- packages/nestjs/src/bull-board.root-module.ts
+++ packages/nestjs/src/bull-board.root-module.ts
@@ -1,20 +1,34 @@
-import { DynamicModule, Inject, MiddlewareConsumer, Module, NestModule, Provider } from "@nestjs/common";
-import { createBullBoard } from "@bull-board/api";
-import { BULL_BOARD_ADAPTER, BULL_BOARD_INSTANCE, BULL_BOARD_OPTIONS } from "./bull-board.constants";
-import { BullBoardModuleAsyncOptions, BullBoardModuleOptions, BullBoardServerAdapter } from "./bull-board.types";
-import { ApplicationConfig, HttpAdapterHost } from "@nestjs/core";
-import { isExpressAdapter, isFastifyAdapter } from "./bull-board.util";
+import { createBullBoard } from '@bull-board/api';
+import {
+  DynamicModule,
+  Inject,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  Provider,
+} from '@nestjs/common';
+import { ApplicationConfig, HttpAdapterHost } from '@nestjs/core';
+
+import {
+  BULL_BOARD_ADAPTER,
+  BULL_BOARD_INSTANCE,
+  BULL_BOARD_OPTIONS,
+} from './bull-board.constants';
+import {
+  BullBoardModuleAsyncOptions,
+  BullBoardModuleOptions,
+  BullBoardServerAdapter,
+} from './bull-board.types';
+import { isExpressAdapter, isFastifyAdapter } from './bull-board.util';
 
 @Module({})
 export class BullBoardRootModule implements NestModule {
-
   constructor(
     private readonly adapterHost: HttpAdapterHost,
     private readonly applicationConfig: ApplicationConfig,
     @Inject(BULL_BOARD_ADAPTER) private readonly adapter: BullBoardServerAdapter,
     @Inject(BULL_BOARD_OPTIONS) private readonly options: BullBoardModuleOptions
-  ) {
-  }
+  ) {}
 
   configure(consumer: MiddlewareConsumer): any {
     const addForwardSlash = (path: string) => {
@@ -35,9 +49,7 @@ export class BullBoardRootModule implements NestModule {
         .getInstance()
         .register(this.adapter.registerPlugin(), { prefix });
 
-        return consumer
-        .apply(this.options.middleware)
-        .forRoutes(this.options.route);
+      return consumer.apply(this.options.middleware).forRoutes(this.options.route);
     }
   }
 
@@ -46,77 +58,63 @@ export class BullBoardRootModule implements NestModule {
 
     const bullBoardProvider: Provider = {
       provide: BULL_BOARD_INSTANCE,
-      useFactory: () => createBullBoard({
-        queues: [],
-        serverAdapter: serverAdapter,
-        options: options.boardOptions,
-      })
+      useFactory: () =>
+        createBullBoard({
+          queues: [],
+          serverAdapter: serverAdapter,
+          options: options.boardOptions,
+        }),
     };
 
     const serverAdapterProvider: Provider = {
       provide: BULL_BOARD_ADAPTER,
-      useFactory: () => serverAdapter
+      useFactory: () => serverAdapter,
     };
 
     const optionsProvider: Provider = {
       provide: BULL_BOARD_OPTIONS,
-      useValue: options
+      useValue: options,
     };
 
     return {
       module: BullBoardRootModule,
       global: true,
       imports: [],
-      providers: [
-        serverAdapterProvider,
-        optionsProvider,
-        bullBoardProvider
-      ],
-      exports: [
-        serverAdapterProvider,
-        bullBoardProvider,
-        optionsProvider
-      ],
+      providers: [serverAdapterProvider, optionsProvider, bullBoardProvider],
+      exports: [serverAdapterProvider, bullBoardProvider, optionsProvider],
     };
   }
 
   static forRootAsync(options: BullBoardModuleAsyncOptions): DynamicModule {
     const bullBoardProvider: Provider = {
       provide: BULL_BOARD_INSTANCE,
-      useFactory: (options: BullBoardModuleOptions, adapter: BullBoardServerAdapter) => createBullBoard({
-        queues: [],
-        serverAdapter: adapter,
-        options: options.boardOptions,
-      }),
-      inject: [BULL_BOARD_OPTIONS, BULL_BOARD_ADAPTER]
+      useFactory: (options: BullBoardModuleOptions, adapter: BullBoardServerAdapter) =>
+        createBullBoard({
+          queues: [],
+          serverAdapter: adapter,
+          options: options.boardOptions,
+        }),
+      inject: [BULL_BOARD_OPTIONS, BULL_BOARD_ADAPTER],
     };
 
     const serverAdapterProvider: Provider = {
       provide: BULL_BOARD_ADAPTER,
       useFactory: (options: BullBoardModuleOptions) => new options.adapter(),
-      inject: [BULL_BOARD_OPTIONS]
+      inject: [BULL_BOARD_OPTIONS],
     };
 
     const optionsProvider: Provider = {
       provide: BULL_BOARD_OPTIONS,
       useFactory: options.useFactory,
-      inject: options.inject
-    }
+      inject: options.inject,
+    };
 
     return {
       module: BullBoardRootModule,
       global: true,
       imports: options.imports,
-      providers: [
-        serverAdapterProvider,
-        optionsProvider,
-        bullBoardProvider
-      ],
-      exports: [
-        serverAdapterProvider,
-        bullBoardProvider,
-        optionsProvider
-      ],
+      providers: [serverAdapterProvider, optionsProvider, bullBoardProvider],
+      exports: [serverAdapterProvider, bullBoardProvider, optionsProvider],
     };
   }
 }
